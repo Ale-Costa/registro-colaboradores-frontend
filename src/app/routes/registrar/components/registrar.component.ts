7bfd9b871bda237e68c8bfa7a5fca846fb579dc0
Ale Costa <alexandrercosta01@gmail.com>
2023-09-02 10:52:29 -0300

    feat: Ajustando tipagem formulario

--- src/app/routes/registrar/components/registrar.component.ts
+++ src/app/routes/registrar/components/registrar.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Colaborador } from 'src/app/shared/models/colaborador.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ColaboradorController } from 'src/app/shared/controllers/colaborador.controller';
-import { map, take } from 'rxjs';
+import { delay, map, take, tap } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { TransformarParametro } from 'src/app/shared/utils/transformar-parametro/transformar-parametro';
 
@@ -11,53 +11,69 @@ type ColaboradorForm = {
   nome: FormControl<string>;
   email: FormControl<string>;
   cpf: FormControl<string>;
-  celular: FormControl<number|null>;
-  conhecimentos: FormControl<string[]>
-}
+  celular: FormControl<string | null>;
+  conhecimentos: FormControl<string[]>;
+};
 
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrls: ['./registrar.component.scss']
+  styleUrls: ['./registrar.component.scss'],
 })
 export class RegistrarComponent {
-
-  formGroup: FormGroup<ColaboradorForm>
-  loadingButton: boolean
-  readonly conhecimentos: string[] = ['Git','React','PHP','NodeJS','DevOps','Banco de Dados','TypeScript']
-
+  formGroup: FormGroup<ColaboradorForm>;
+  loadingButton: boolean;
+  readonly conhecimentos: string[] = ['Git', 'React', 'PHP', 'NodeJS', 'DevOps', 'Banco de Dados', 'TypeScript'];
 
   constructor(
-    private readonly colaboradorService: ColaboradorController,
+    private readonly colaboradorController: ColaboradorController,
     private readonly fb: FormBuilder,
     private readonly matSnackBar: MatSnackBar,
     private readonly route: ActivatedRoute
-    ) {
-    this.formGroup = this.buildFormGroup()
+  ) {
+    this.formGroup = this.buildFormGroup();
   }
 
-  private buildFormGroup():FormGroup {
-    const nomeColaborador = this.route.snapshot.paramMap.get('colaborador') || ''
-    const nomeLegivel = TransformarParametro.transformarParaNomeLegivel(nomeColaborador)
+  private buildFormGroup(): FormGroup {
+    const nomeColaborador = this.route.snapshot.paramMap.get('colaborador') || '';
+    const nomeLegivel = TransformarParametro.transformarParaNomeLegivel(nomeColaborador);
 
     return this.fb.group({
-      nome: [nomeLegivel,[Validators.required]],
-      email: ['',[Validators.required, Validators.email]],
-      cpf: ['',[Validators.required]],
+      nome: [nomeLegivel, [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      cpf: ['', [Validators.required]],
       celular: [],
-      conhecimentos: ['',[Validators.required]]
-    })
-  }
-
-  private get colaborador (): Colaborador{
-    return this.formGroup.value as Colaborador
-  }
-
-  registrar(){
-    this.loadingButton = true
-    this.colaboradorService.registrarColaborador(this.colaborador).pipe( take(1)).subscribe({
-        next: () => this.matSnackBar.open('Registrado com sucesso','OK',{duration: 5000}),
-        error: erro => this.matSnackBar.open(erro,'OK',{duration: 10000}),
+      conhecimentos: ['', [Validators.required]],
     });
+  }
+
+  private get colaborador(): Colaborador {
+    return this.formGroup.value as Colaborador;
+  }
+  buscar() {
+    this.colaboradorController.buscarTodos().subscribe();
+  }
+
+  registrar() {
+    this.loadingButton = true;
+    this.colaboradorController
+      .registrarColaborador(this.colaborador)
+      .pipe(
+        take(1),
+        tap({
+          error: () => (this.loadingButton = false),
+          complete: () => (this.loadingButton = false),
+        })
+      )
+      .subscribe({
+        next: () =>
+          this.matSnackBar.open('Registrado com sucesso', 'OK', {
+            duration: 5000,
+          }),
+        error: ({ error }) => {
+          const erro = error.message || 'Erro desconhecido';
+          this.matSnackBar.open(erro, 'OK', { duration: 10000 });
+        },
+      });
   }
 }
--- src/app/shared/controllers/colaborador.controller.ts
+++ src/app/shared/controllers/colaborador.controller.ts
@@ -1,20 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Colaborador } from '../models/colaborador.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColaboradorController {
+  private readonly baseUrl = 'http://localhost:3000/colaborador';
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
-  registrarColaborador(colaborador: Colaborador): Observable<Colaborador>{
-    return this.http.post<Colaborador>('/api/colaborador', colaborador);
+  registrarColaborador(colaborador: Colaborador): Observable<Colaborador> {
+    return this.http.post<Colaborador>(`${this.baseUrl}/registrar`, colaborador);
   }
 
-  validarColaborador(colaborador: Colaborador): Observable<Colaborador>{
-    return this.http.post<Colaborador>(`/api/colaborador/${colaborador.cpf}`, colaborador);
+  validarColaborador(id: number): Observable<Colaborador> {
+    return this.http.put<Colaborador>(`${this.baseUrl}/validar/${id}`, {});
+  }
+
+  buscarTodos(): Observable<Colaborador[]> {
+    return this.http.get<Colaborador[]>(`${this.baseUrl}`);
   }
 }
